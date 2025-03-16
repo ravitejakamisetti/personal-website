19f1da0ed67b20dbcf5aac3d71496a11cac55910
ravitejakamisetti <contact.ravi@icloud.com>
2025-03-16 17:17:06 +0530

    added animations

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const elementsToAnimate = document.querySelectorAll('.animate, #about-contact');
     const navbarToggle = document.querySelector('.navbar-toggle');
     const navbarLinks = document.querySelector('.navbar-links');
+    const navLinks = document.querySelectorAll('.navbar-links a');
 
     const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
+        entries.forEach((entry, index) => {
             if (entry.isIntersecting) {
+                entry.target.style.animationDelay = `${index * 0.2}s`;
                 entry.target.classList.add('fade-in');
                 observer.unobserve(entry.target);
             }
@@ -16,5 +18,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     navbarToggle.addEventListener('click', () => {
         navbarLinks.classList.toggle('active');
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            if (link.hash) {
+                event.preventDefault();
+                const target = document.querySelector(link.hash);
+                target.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
     });
 });
